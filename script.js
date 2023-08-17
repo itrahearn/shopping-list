58741d4ab3b8775075351a48f84110d4bba9d8fe
Ian Trahearn <itrahearn@wiley.com>
2023-08-17 20:40:01 +0100

    next chapter

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const itemClear = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e)
 {
@@ -23,6 +26,7 @@ function addItem(e)
         li.appendChild(button);
         itemList.appendChild(li);
         itemInput.value = '';
+        checkUI();
     }
     
 }
@@ -49,8 +53,43 @@ function createIcon(classes){
     debugger;
     const i = document.createElement('i');
     i.className = classes;
+    i.addEventListener("click", (e)=> {
+
+    });
     return i;
 }
 // Event Listeners
 
-itemForm.addEventListener('submit', addItem)
+function removeItem(e){
+    if(e.target.parentElement.classList.contains("remove-item"))
+    {
+        if(window.confirm("Are you sure you want to delete the item?"))
+            e.target.parentElement.parentElement.remove();
+        checkUI();
+    }
+    
+}
+function removeAllItems()
+{
+    while(itemList.firstChild)
+    {
+        itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+}
+
+function checkUI(){
+
+    const items = itemList.querySelectorAll('li');
+    // there are no node items
+    if(items.length === 0){
+        itemFilter.style.display = itemClear.style.display="none";
+    } else {
+        itemFilter.style.display = itemClear.style.display="block";
+    }
+}
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+itemClear.addEventListener('click', removeAllItems);
+
+checkUI();
